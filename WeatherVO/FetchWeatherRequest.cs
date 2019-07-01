b5f17ff2ed04457787ac39196753851e25b83f9e
FGpramodgupta <51231047+FGpramodgupta@users.noreply.github.com>
2019-07-01 20:31:54 +0530

    Add files via upload

---
+++ WeatherVO/FetchWeatherRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherVO
+{
+    public class FetchWeatherRequest
+    {
+        public string City { get; set; }
+
+        //string objCity;
+        //public string City
+        //{
+        //    get { return objCity; }
+        //    set { objCity = value; }
+        //}
+    }
+}
---
+++ WeatherVO/FetchWeatherResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherVO
+{
+    public class FetchWeatherResponse
+    {
+
+        //string objErrorCode;
+        //public string ErrorCode
+        //{
+        //    get { return objErrorCode; }
+        //    set { objErrorCode = value; }
+        //}
+
+        //string objErrorDescription;
+        //public string ErrorDescription
+        //{
+        //    get { return objErrorDescription; }
+        //    set { objErrorDescription = value; }
+        //}
+        //ResponseWeather objResponseWeather;
+        //public ResponseWeather ResponseWeather
+        //{
+        //    get { return objResponseWeather; }
+        //    set { objResponseWeather = value; }
+        //}
+
+        public string ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+        public ResponseWeather ResponseWeather { get; set; }
+
+    }
+}
