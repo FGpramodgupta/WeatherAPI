8d6a717e1553c2981e5d3626f66b5e4f05a527c0
FGpramodgupta <51231047+FGpramodgupta@users.noreply.github.com>
2019-07-01 20:23:54 +0530

    Add files via upload

---
+++ WeatherBO/WeatherBAL.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using WeatherVO;
+using System.Web;
+ 
+
+namespace WeatherBO
+{
+    public class WeatherBAL
+    {
+
+        public FetchWeatherResponse FetchWeatherByID(string cityID)
+        {
+            FetchWeatherResponse fetchWeatherResponse = new FetchWeatherResponse();
+
+            ResponseWeather responseWeather;
+            /*Calling API http://openweathermap.org/api */
+            string apiKey = ConfigurationManager.AppSettings["AppID"].ToString();
+            HttpWebRequest apiRequest =
+            WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?id=" +
+            cityID + "&appid=" + apiKey + "&units=metric") as HttpWebRequest;
+
+            string apiResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+            responseWeather = JsonConvert.DeserializeObject<ResponseWeather>(apiResponse);
+            fetchWeatherResponse.ErrorCode = "0";
+            fetchWeatherResponse.ErrorDescription = "";
+            fetchWeatherResponse.ResponseWeather = responseWeather;
+            return fetchWeatherResponse;
+        }
+
+    }
+}
